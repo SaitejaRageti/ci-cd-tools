374ba28d594ae71d969954ce5659eadf6cf18281
saitejarageti <saitejarageti129@gmail.com>
2025-09-02 16:26:39 +0530

    jenkins-terraform

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "rageti.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z10087322TJIFOPYCZ97S"
+  description = "description"
+}
